2ca93aea04cabe4cfb2c6420264232b6515a158b
norbert-kulus-blockydevs <norbert.kulus@blockydevs.com>
2025-02-07 12:52:16 +0100

    [QLCW-22](chore): remove commented code and unused unsupported package

--- src/components/kaspa-qrcode.tsx
+++ src/components/kaspa-qrcode.tsx
@@ -1,22 +1,4 @@
-// import { QRCode } from 'react-qrcode-logo';
-
 export default function KaspaQrCode(_: any) {
     // The package 'react-qrcode-logo' won't work on react 19, use this package as alternative https://github.com/zpao/qrcode.react
     return <></>;
-    // return (
-    //     <QRCode
-    //         value={props.value}
-    //         fgColor='#134a40'
-    //         eyeRadius={[
-    //             [10, 10, 0, 10], // top/left eye
-    //             [10, 10, 10, 0], // top/right eye
-    //             [10, 0, 10, 10], // bottom/left
-    //         ]}
-    //         eyeColor={[
-    //             { outer: '#134a40', inner: '#49EACB' },
-    //             { outer: '#134a40', inner: '#49EACB' },
-    //             { outer: '#134a40', inner: '#49EACB' },
-    //         ]}
-    //     />
-    // );
 }
