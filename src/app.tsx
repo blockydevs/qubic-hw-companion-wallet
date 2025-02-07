fac35f7c3da3462fe6b1ece95d3c1af17e89cebe
norbert-kulus-blockydevs <norbert.kulus@blockydevs.com>
2025-02-07 12:55:11 +0100

    [QLCW-22](feat): Create NavbarContent component

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,18 @@
-import { ColorSchemeScript, Divider } from '@mantine/core';
+import { ColorSchemeScript } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
-import { Outlet, Route, Routes as RouterRoutes, useLocation } from 'react-router';
-import { NavbarLink } from './components/navbar-link';
+import { Outlet, Route, Routes as RouterRoutes } from 'react-router';
 import { Layout } from './layout/Layout';
+import { NavbarContent } from './layout/NavbarContent';
 import { AppMantineProvider } from './providers/AppMantineProvider';
+import { DashboardContextProvider } from './providers/DashboardContextProvider';
+import { DeviceTypeProvider } from './providers/DeviceTypeProvider';
+import { RequireDeviceTypeProvider } from './providers/RequireDeviceTypeProvider';
 import Home from './routes/home/page';
 import { WalletAddressesPage } from './routes/wallet/addresses/page';
 import { WalletOverviewPage } from './routes/wallet/overview/page';
 import { WalletTransactionsPage } from './routes/wallet/transactions/page';
-import { DeviceTypeProvider } from './providers/DeviceTypeProvider';
-import { RequireDeviceTypeProvider } from './providers/RequireDeviceTypeProvider';
-import { DashboardContextProvider } from './providers/DashboardContextProvider';
 
 export default function App() {
-    const { pathname } = useLocation();
-    const showWalletDashboardMenu = pathname.startsWith('/wallet');
-
     return (
         <>
             <ColorSchemeScript />
@@ -23,33 +20,7 @@ export default function App() {
             <AppMantineProvider>
                 <Notifications />
 
-                <Layout
-                    navbarContent={
-                        showWalletDashboardMenu ? (
-                            <>
-                                <NavbarLink
-                                    icon='ManageHistory'
-                                    to='/'
-                                    label='Go back to device type selection'
-                                />
-
-                                <Divider />
-
-                                <NavbarLink
-                                    to='/wallet/addresses'
-                                    icon='ImportContacts'
-                                    label='Addresses'
-                                />
-                                <NavbarLink to='/wallet/overview' icon='Face5' label='Overview' />
-                                <NavbarLink
-                                    to='/wallet/transactions'
-                                    icon='Bolt'
-                                    label='Transactions'
-                                />
-                            </>
-                        ) : null
-                    }
-                >
+                <Layout navbarContent={<NavbarContent />}>
                     <DeviceTypeProvider>
                         <RouterRoutes>
                             <Route path='/' element={<Home />} />
