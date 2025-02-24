c752319fbcc15fd884546e8c69d30db809d0928a
norbert-kulus-blockydevs <norbert.kulus@blockydevs.com>
2025-02-24 16:41:49 +0100

    [QLCW-41](chore): change prop to shorter one

--- src/components/SensitiveDataWrapper.tsx
+++ src/components/SensitiveDataWrapper.tsx
@@ -2,19 +2,16 @@ import { Stack } from '@mantine/core';
 import type { BoxComponentProps, PolymorphicComponentProps } from '@mantine/core';
 
 interface SensitiveDataWrapperProps extends PolymorphicComponentProps<'div', BoxComponentProps> {
-    isSensitiveDataHidden: boolean;
+    isHidden: boolean;
 }
 
-export const SensitiveDataWrapper = ({
-    children,
-    isSensitiveDataHidden,
-}: SensitiveDataWrapperProps) => {
+export const SensitiveDataWrapper = ({ children, isHidden }: SensitiveDataWrapperProps) => {
     return (
         <Stack
             pos='relative'
             style={{
                 overflow: 'visible',
-                filter: `blur(${isSensitiveDataHidden ? 13 : 0}px)`,
+                filter: `blur(${isHidden ? 13 : 0}px)`,
                 transition: ' 0.13s filter ease',
             }}
         >
